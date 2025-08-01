f28365660b92b9b181f661b1c65c94521c62459f
Prince Sen <mikesegur8@gmail.com>
2025-08-01 09:59:33 +0530

    Updating home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -693,7 +693,7 @@ const LandingPage = () => {
           </div>
           
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2024 InterviewAI. All rights reserved.</p>
+            <p>&copy; 2025 Prepview. All rights reserved.</p>
           </div>
         </div>
       </footer>
